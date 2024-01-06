252465f7af52211710a547ba836534f0661606bd
Peiyun <b612starnight@gmail.com>
2024-01-07 00:18:25 +0800

    feat:addNabigationBarItem

---
+++ EasyBizLedger/Extension/UINavigationController+Extension.swift
@@ -0,0 +1,43 @@
+//
+//  UINavigationController+Extension.swift
+//  EasyBizLedger
+//
+//  Created by Peiyun on 2023/12/26.
+//
+
+import UIKit
+
+extension UINavigationController {
+    static var home: UINavigationController {
+        return addNavigationController(for: HomeViewController(), title: "home")
+    }
+    static var add: UINavigationController {
+        return addNavigationController(for: AddViewController(), title: "add")
+    }
+    static var list: UINavigationController {
+        return addNavigationController(for: ListViewController(), title: "list")
+    }
+    
+    private static func addNavigationController(
+        for rootViewController: UIViewController,
+        title: String?
+    ) -> UINavigationController {
+
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+//        let barApperance = UINavigationBarAppearance()
+//        barApperance.shadowColor = UIColor.clear
+//        navigationController.navigationBar.layer.opacity = 0.9
+//        navigationController.navigationBar.layer.backgroundColor = UIColor.clear.cgColor
+//
+//        navigationController.navigationBar.prefersLargeTitles = true
+//
+//        navigationController.navigationItem.largeTitleDisplayMode = .automatic
+//        navigationController.navigationBar.largeTitleTextAttributes = [
+//            .foregroundColor: UIColor.setColor(lightColor: .darkGray, darkColor: .white),
+//            .font: UIFont.boldSystemFont(ofSize: 28)
+//        ]
+
+        //rootViewController.navigationItem.title = title
+        return navigationController
+    }
+}
---
+++ EasyBizLedger/View/TabBarViewController.swift
@@ -0,0 +1,99 @@
+//
+//  TabBarViewController.swift
+//  EasyBizLedger
+//
+//  Created by Peiyun on 2023/12/25.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    private let tabs: [Tab] = [.home, .add, .list]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.tabBar.tintColor = .orange
+        viewControllers = tabs.map { $0.makeViewController() }
+        let barAppearance =  UITabBarAppearance()
+        barAppearance.configureWithDefaultBackground()
+        UITabBar.appearance().scrollEdgeAppearance = barAppearance
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - Tabs
+extension TabBarViewController {
+    private enum Tab {
+        case home
+        case add
+        case list
+
+        func makeViewController() -> UIViewController {
+            let controller: UIViewController
+            switch self {
+            case .home: controller = UINavigationController.home
+            case .add: controller = UINavigationController.add
+            case .list: controller = UINavigationController.list
+            }
+            controller.tabBarItem = makeTabBarItem()
+            return controller
+        }
+        
+        private func makeTabBarItem() -> UITabBarItem {
+            return UITabBarItem(title: title, image: image, selectedImage: selectedImage)
+        }
+        
+        private var title: String {
+            switch self {
+            case .home:
+                return "首頁"
+            case .add:
+                return "新增"
+            case .list:
+                return "我的清單"
+            }
+        }
+        private var image: UIImage? {
+            switch self {
+            case .home:
+                return .systemAsset(.house)
+            case .add:
+                return .systemAsset(.magnifyingglass)
+            case .list:
+                return  .systemAsset(.heart)
+            }
+        }
+        private var selectedImage: UIImage? {
+            switch self {
+            case .home:
+                return .systemAsset(.selectedHouse)
+            case .add:
+                return .systemAsset(.magnifyingglass)
+            case .list:
+                return .systemAsset(.selectedHeart)
+            }
+        }
+    }
+}
+
+extension TabBarViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(
+        _ tabBarController: UITabBarController,
+        shouldSelect viewController: UIViewController
+    ) -> Bool {
+        return true
+    }
+}
